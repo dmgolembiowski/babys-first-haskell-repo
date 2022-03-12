f07999da43319dd50b9047426868f90dba644d69
David Golembiowski <david@dgolembiowski.com>
2022-03-12 16:57:50 -0500

    concatenating lists of `Char`

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,12 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+      firstOrEmpty,
     ) where
-
 someFunc :: IO ()
 someFunc = putStrLn "hello, Haskell world!"
+firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty lst = if not (null lst) then head lst else "empty"
+lst1 +++ lst2 = if null lst1
+                then lst2
+                else (head lst1) : (tail lst1 +++ lst2)
+
